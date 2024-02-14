9bedd27e16ea5f444952b87b377a1338e85379bf
Berk <berkkarakas98@hotmail.com>
2024-02-14 15:48:30 +0300

    Fix jump button multiple press bug
    - Prevent repeated jumping when the jump button is pressed consecutively, regardless of the checking grounded state or other conditions.

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,24 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    public Vector2 horizontalInput { get; private set; }
+    public Vector2 HorizontalInput { get; private set; }
     public bool JumpInput { get; private set; }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        horizontalInput = context.ReadValue<Vector2>();
+        HorizontalInput = context.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
         if (context.started)
-        {
             JumpInput = true;
-        }
+
+        if (context.canceled)
+            JumpInput = false;
     }
 
     public void JumpInputHelper() => JumpInput = false;
